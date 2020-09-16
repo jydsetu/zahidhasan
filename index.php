b412795d685084c53dd23da366ac70ba5f64760c
jydsetu <59053563+jydsetu@users.noreply.github.com>
2020-09-16 18:22:40 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Desktop site</title>
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+<link rel="stylesheet" href="index.css">
+</head>
+<body>
+
+<div id="kkk"  class="bg-primary">
+<span>Home</span>
+<span>About</span>
+<span>Contact</span>
+<span>Blog</span>
+</div>
+
+<div id="manuside"  class="bg-success" >
+<p>Yoshihide</p>
+<p>Rlected</p>
+<p>Elected</p>
+<p>Televised</p>
+<p>Contradicts</p>
+<p>Comments</p>
+<p>Completed</p>
+<p>Tuesday</p>
+<p>Vaccine</p>
+</div>
+
+<div id="page" >
+<h3>Yoshihide Suga was elected</h3>
+<p>Yoshihide Suga was elected as Japan's next prime minister by the country's lower house of parliament on Wednesday, replacing Shinzo Abe who resigned because of poor health and promising to continue his signature economic policies.The 71-year-old Suga - the son of a strawberry farmer - won the leadership contest of the governing Liberal Democratic Party (LDP) on Monday and was all but assured the top job when his appointment went to a vote in parliament given the party's two-thirds majority.</p>
+<h3>At a televised event with voters</h3>
+<p>At a televised event with voters, Mr Trump said he had "up-played" it.
+The claim contradicts comments Mr Trump made to journalist Bob Woodward earlier this year, when he said he minimised the virus's severity to avoid panic.
+Mr Trump also repeated on Tuesday that a vaccine could be ready "within weeks" despite scepticism from health experts.
+No vaccine has yet completed clinical trials, leading some scientists to fear politics rather than health and safety is driving the push for a vaccine before the 3 November presidential elections.</p>
+
+
+<h3>Yoshihide Suga was elected</h3>
+<p>Yoshihide Suga was elected as Japan's next prime minister by the country's lower house of parliament on Wednesday, replacing Shinzo Abe who resigned because of poor health and promising to continue his signature economic policies.The 71-year-old Suga - the son of a strawberry farmer - won the leadership contest of the governing Liberal Democratic Party (LDP) on Monday and was all but assured the top job when his appointment went to a vote in parliament given the party's two-thirds majority.</p>
+<h3>At a televised event with voters</h3>
+<p>At a televised event with voters, Mr Trump said he had "up-played" it.
+The claim contradicts comments Mr Trump made to journalist Bob Woodward earlier this year, when he said he minimised the virus's severity to avoid panic.
+Mr Trump also repeated on Tuesday that a vaccine could be ready "within weeks" despite scepticism from health experts.
+No vaccine has yet completed clinical trials, leading some scientists to fear politics rather than health and safety is driving the push for a vaccine before the 3 November presidential elections.</p>
+
+</div>
+<div id="oh"  class="bg-primary">
+<span>Home</span>
+<span>About</span>
+<span>Contact</span>
+<span>Blog</span>
+</div>
+</body>
+</html>
